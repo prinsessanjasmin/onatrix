2be8e426073c7e0952bd17a1889af779e45bb039
Jasmin de Freitas <jasmindefreitas@hotmail.com>
2025-11-03 16:23:20 +0100

    Add extra service to make slider work on desktop screen. Add Success examples to Success stories in Umbraco.

--- Onatrix/umbraco/models/SuccessItem.generated.cs
+++ Onatrix/umbraco/models/SuccessItem.generated.cs
@@ -18,14 +18,14 @@ using Umbraco.Extensions;
 
 namespace Umbraco.Cms.Web.Common.PublishedModels
 {
-	/// <summary>Success Story Section</summary>
-	[PublishedModel("successStorySection")]
-	public partial class SuccessStorySection : PublishedElementModel
+	/// <summary>Success Item</summary>
+	[PublishedModel("successItem")]
+	public partial class SuccessItem : PublishedElementModel
 	{
 		// helpers
 #pragma warning disable 0109 // new is redundant
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.2.0+4eae48e")]
-		public new const string ModelTypeAlias = "successStorySection";
+		public new const string ModelTypeAlias = "successItem";
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.2.0+4eae48e")]
 		public new const PublishedItemType ModelItemType = PublishedItemType.Content;
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.2.0+4eae48e")]
@@ -34,14 +34,14 @@ namespace Umbraco.Cms.Web.Common.PublishedModels
 			=> PublishedModelUtility.GetModelContentType(contentTypeCache, ModelItemType, ModelTypeAlias);
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.2.0+4eae48e")]
 		[return: global::System.Diagnostics.CodeAnalysis.MaybeNull]
-		public static IPublishedPropertyType GetModelPropertyType<TValue>(IPublishedContentTypeCache contentTypeCache, Expression<Func<SuccessStorySection, TValue>> selector)
+		public static IPublishedPropertyType GetModelPropertyType<TValue>(IPublishedContentTypeCache contentTypeCache, Expression<Func<SuccessItem, TValue>> selector)
 			=> PublishedModelUtility.GetModelPropertyType(GetModelContentType(contentTypeCache), selector);
 #pragma warning restore 0109
 
 		private IPublishedValueFallback _publishedValueFallback;
 
 		// ctor
-		public SuccessStorySection(IPublishedElement content, IPublishedValueFallback publishedValueFallback)
+		public SuccessItem(IPublishedElement content, IPublishedValueFallback publishedValueFallback)
 			: base(content, publishedValueFallback)
 		{
 			_publishedValueFallback = publishedValueFallback;
@@ -50,35 +50,18 @@ namespace Umbraco.Cms.Web.Common.PublishedModels
 		// properties
 
 		///<summary>
-		/// Example
+		/// Description
 		///</summary>
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.2.0+4eae48e")]
 		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
-		[ImplementPropertyType("successStoryExample")]
-		public virtual global::Umbraco.Cms.Core.Models.Blocks.BlockListModel SuccessStoryExample => this.Value<global::Umbraco.Cms.Core.Models.Blocks.BlockListModel>(_publishedValueFallback, "successStoryExample");
+		[ImplementPropertyType("successDescription")]
+		public virtual string SuccessDescription => this.Value<string>(_publishedValueFallback, "successDescription");
 
 		///<summary>
-		/// Headline 
+		/// Number
 		///</summary>
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.2.0+4eae48e")]
-		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
-		[ImplementPropertyType("successStoryHeadline")]
-		public virtual string SuccessStoryHeadline => this.Value<string>(_publishedValueFallback, "successStoryHeadline");
-
-		///<summary>
-		/// Image 
-		///</summary>
-		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.2.0+4eae48e")]
-		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
-		[ImplementPropertyType("successStoryImage")]
-		public virtual global::Umbraco.Cms.Core.Models.MediaWithCrops SuccessStoryImage => this.Value<global::Umbraco.Cms.Core.Models.MediaWithCrops>(_publishedValueFallback, "successStoryImage");
-
-		///<summary>
-		/// Label
-		///</summary>
-		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.2.0+4eae48e")]
-		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
-		[ImplementPropertyType("successStoryLabel")]
-		public virtual string SuccessStoryLabel => this.Value<string>(_publishedValueFallback, "successStoryLabel");
+		[ImplementPropertyType("successNumber")]
+		public virtual int SuccessNumber => this.Value<int>(_publishedValueFallback, "successNumber");
 	}
 }
