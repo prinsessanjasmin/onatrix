f35cc87e8519d063dda032367b32cb8989decda5
Jasmin de Freitas <jasmindefreitas@hotmail.com>
2025-11-10 09:08:14 +0100

    Make EmployeeSection responsive (add slider for mobile and tablet)

--- Onatrix/wwwroot/js/swiper.js
+++ Onatrix/wwwroot/js/swiper.js
@@ -40,4 +40,30 @@
             }
         }
     })
+
+    const employeeSwiper = new Swiper('#employeeSwiper', {
+        slidesPerView: 1.1,
+        spaceBetween: 8,
+        centeredSlides: true,
+        centeredSlidesBounds: true,
+        slidesOffsetBefore: 8,
+        slidesOffsetAfter: 8,
+
+        navigation: {
+            nextEl: '.employee-next',
+            prevEl: '.employee-prev'
+        },
+
+        breakpoints: {
+            768: {
+                slidesPerView: 2.1,
+                spaceBetween: 8,
+                slidesPerGroup: 2,
+                centeredSlides: false,
+                centeredSlidesBounds: false,
+                slidesOffsetBefore: 8,
+                slidesOffsetAfter: 8,
+            }
+        }
+    })
 })
